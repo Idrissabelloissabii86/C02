c78f91e28a6be436d2e6287f34d4222f172fd004
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-18 23:25:27 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -7,7 +7,7 @@ int main(){
 
     // verifie si le nombre est pair ou impair
     if (n % 2 == 0){
-        printf("%d est un nombre pair", n);
+        printf("%d est un nombre pair.", n);
     }else
     {
         printf("%d est un nombre impair.", n);
