90712f294910db28aed60ea136d513c5e7e1668d
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-19 14:09:32 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+
 int main() {
-    // récupération de l'entier et opération
     int n;
-    
+    int quotient, reste;
+
+    // Lire un entier
+    printf("Entrez un entier : ");
     scanf("%d", &n);
-    
-    // verification de l'entier
-        if (n / 2 && n % 2 == 0){
-            printf("%d est un nombre pair.\n", n);
-        }else{
-            printf("%d est un nombre impair.\n", n);
-        }
+
+    // Division entière par 2
+    quotient = n / 2;
+    reste = n % 2;
+
+    // Affichage du quotient
+    printf("Résultat de la division entière par 2 : %d\n", quotient);
+
+    // Vérification du modulo
+    if (reste == 0)
+        printf("%d est un nombre pair.\n", n);
+    else
+        printf("%d est un nombre impair.\n", n);
+
     return 0;
 }
