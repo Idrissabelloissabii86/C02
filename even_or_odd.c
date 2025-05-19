eebeecfa3edfc2de5d673e6acac5d9f5054b5e80
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-19 15:47:08 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int main() {
-    int n;
-    // Lire un entier
-    scanf("%d", &n);
-    
-    // Vérification du modulo
-    if (n % 2 = 0){
-        printf("%d est pair.\n", n);
+int main(){
+    // récupération de la valeur
+    int a;       
+    scanf("%d", &a);
+    // condition de vérificationn du nombre (pair ou impair)
+    if (a % 2 == 0){
+        printf("%d est pair.\n", a);
     }else{
-        printf("%d est impair.\n", n);
+        printf("%d est impair.\n", a);
     }
-    return 0;
+    
+return 0;
 }
